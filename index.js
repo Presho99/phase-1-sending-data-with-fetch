f02631ab48dc944688a3ebb49a80cd463b64a437
Precious <Preshomondi@gmail.com>
2022-06-20 00:52:57 +0300

    commit tree 8d53ceac15664b64c4bda71db36a2321010f0170

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData (name, email) {
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then((obj) => {
+        return document.body.innerHTML = obj.id
+    })
+    .catch((error) => {
+        
+        return document.body.innerHTML = error.message
+    })
+    
+}
